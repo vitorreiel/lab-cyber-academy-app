037d8fa3fe6adb5aafe7a0b114bba64618b3a514
Vitor Reiel <vitorreiel@hotmail.com>
2023-10-28 16:57:34 -0300

    feat: clear events in terminal

--- public/src/App.js
+++ public/src/App.js
@@ -7,52 +7,52 @@ const CONTAINERS_LIST = [
   {
     id: 1,
     name: "Containernet",
-    command: "docker exec -it containernet bash"
+    command: "clear && docker exec -it containernet bash"
   },
   {
     id: 2,
     name: "Suporte",
-    command: "docker exec -it mn.suporte bash"
+    command: "clear && docker exec -it mn.suporte bash"
   },
   {
     id: 3,
     name: "Internet",
-    command: "docker exec -it mn.internet bash"
+    command: "clear && docker exec -it mn.internet bash"
   },
   {
     id: 4,
     name: "Server Web",
-    command: "docker exec -it mn.servWeb bash"
+    command: "clear && docker exec -it mn.servWeb bash"
   },
   {
     id: 5,
     name: "Server Samba",
-    command: "docker exec -it mn.servSamba bash"
+    command: "clear && docker exec -it mn.servSamba bash"
   },
   {
     id: 6,
     name: "User Gustavo",
-    command: "docker exec -it mn.gustavo bash"
+    command: "clear && docker exec -it mn.gustavo bash"
   },
   {
     id: 7,
     name: "User Joyce",
-    command: "docker exec -it mn.joyce bash"
+    command: "clear && docker exec -it mn.joyce bash"
   },
   {
     id: 8,
     name: "User Henrique",
-    command: "docker exec -it mn.henrique bash"
+    command: "clear && docker exec -it mn.henrique bash"
   },
   {
     id: 9,
     name: "User Gabriele",
-    command: "docker exec -it mn.gabriele bash"
+    command: "clear && docker exec -it mn.gabriele bash"
   },
   {
     id: 10,
     name: "User João",
-    command: "docker exec -it mn.joao bash"
+    command: "clear && docker exec -it mn.joao bash"
   }
 ];
 
